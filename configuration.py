438b901ee2a415530775d4c26f4d371636155324
joanafo <132621765+joanafo@users.noreply.github.com>
2024-03-17 12:38:18 -0700

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = 'https://cnt-61c309db-08b8-4892-bd8d-f9afb0193723.containerhub.tripleten-services.com'
+DOC_PATH = '/docs/'
+LOG_MAIN_PATH = '/api/logs/main/'
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ sender_stand_request.py
@@ -0,0 +1,28 @@
+import configuration
+import requests
+import data
+
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+
+response = post_new_user(data.user_body)
+print(response.status_code)
+print(response.json())
+
+
+def post_products_kits(products_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,
+                         json=products_ids,  # Datos a enviar en la solicitud.
+                         headers=data.headers)  # Encabezados de solicitud.
+
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+
+response = get_users_table()
+print(response.status_code)
